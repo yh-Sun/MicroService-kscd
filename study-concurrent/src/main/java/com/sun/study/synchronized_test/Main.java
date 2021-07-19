1de42c48c109a73859934db4e91d9a9487e4c202
小太阳 <sunyuanhe_i@163.com>
2021-07-19 16:46:32 +0800

    Synchronized锁 静态方法、实例方法

---
+++ study-concurrent/src/main/java/com/sun/study/synchronized_test/Main.java
@@ -0,0 +1,36 @@
+package com.sun.study.synchronized_test;
+
+import lombok.SneakyThrows;
+
+/**
+ * Synchronized锁 静态方法、实例方法
+ *
+ * @author  Sun
+ * @date    2021/7/19 16:45
+ * @since   1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SynchronizedTest test = new SynchronizedTest();
+
+        new Thread(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                SynchronizedTest.syncStatic();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                test.sync();
+            }
+        }).start();
+
+
+    }
+
+}
---
+++ study-concurrent/src/main/java/com/sun/study/synchronized_test/SynchronizedTest.java
@@ -0,0 +1,15 @@
+package com.sun.study.synchronized_test;
+
+public class SynchronizedTest {
+
+    public synchronized static void syncStatic() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(200000000);
+    }
+
+    public synchronized void sync() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(200000000);
+    }
+
+}
