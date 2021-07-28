21d7a4f6834335b0c098a296a4cf9d88df85dbb0
小太阳 <sunyuanhe_i@163.com>
2021-07-28 10:48:27 +0800

    最近代码提交

--- study-concurrent/src/main/java/com/sun/study/Main.java
+++ study-concurrent/src/main/java/com/sun/study/Main.java
@@ -1,33 +1,13 @@
 package com.sun.study;
 
-import lombok.Data;
-
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.locks.ReentrantLock;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 
 public class Main {
 
     public static void main(String[] args) {
-        Optional<A> a = Optional.of(new A());
-        String b1 = Optional.of(a.get().getB().getB1()).orElseThrow(null);
-        System.out.println(b1);
-        ReentrantLock roke = new ReentrantLock();
-    }
-
-    @Data
-    static class A {
-        private String a1;
-        private String a2;
-        private String a3;
-        private String a4;
-        private B b;
-    }
-
-    @Data
-    class B {
-        private String b1;
+        DateTime parse = DateUtil.parse("2021-04-22 12:30:23");
+        System.out.println(parse);
     }
 
 }
